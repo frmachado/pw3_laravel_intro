46ab1072de5bb6d962dd51b43c5157fb6ced9b56
Fabricio Machado <fabricioriba1234@gmail.com>
2026-05-20 16:45:46 -0300

    commit: tela

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
     //
+    public function index()
+    {
+        $produtos = Produto::orderBy('nome')->get();
+        return view('produtos.index', compact('produtos'));
+    }
+    public function store(Request $request)
+    {
+        $dados = $request->validate([
+            'nome' => 'required|min:3',
+            'preco' => 'required|numeric|min:3',
+            'estoque' => 'required|interger|min:0'
+        ]);
+
+        Produto::create($dados);
+
+        return redirect('/produtos');
+
+    }
 }
---
+++ resources/views/produtos/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+</head>
+<body>
+    <h1>Cadastro de Produtos</h1>
+
+    <form action="/produtos" method="post">
+        @call_user_func
+
+        <label for="nome">Nome</label>
+        <input type="text" id="nome" name="nome" require><br><br>
+
+        <label for="preco">Preço</label>
+        <input type="text" step="0.01" id="preco" name="preco" require><br><br>
+
+        <label for="estoque">Estoque</label>
+        <input type="text" id="estoque" name="estoque" require><br><br>
+        
+        <button type="submit">Salvar</button>
+
+    </form>
+
+    <h2>Lista de Produtos</h2>
+
+    @if($produtos->isEmpyt())
+    @else
+    @endif
+
+</body>
+</html>
