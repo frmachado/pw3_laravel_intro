f147ab5590eb88ea3b2b68eb90f3ad7bb85c7674
Fabricio Machado <fabricioriba1234@gmail.com>
2026-08-12 16:46:08 -0300

    landing page com tailwind

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
@@ -7,5 +6,8 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::view('/landing','landing');
+
 Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::post('/produtos', [ProdutoController::class, 'store']);
+
