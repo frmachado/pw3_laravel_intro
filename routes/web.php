491f68b0c7af2c85335b9d78f4c22a577b272a45
Fabricio Machado <fabricioriba1234@gmail.com>
2026-08-28 17:34:05 -0300

    atualizacao

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Models\User;
 use App\Http\Controllers\LivroController;
 
 Route::get('/livros', [LivroController::class, 'index'])
@@ -6,3 +7,15 @@ Route::get('/livros', [LivroController::class, 'index'])
 
 Route::post('/livros', [LivroController::class, 'store'])
     ->name('livros.store');
+
+Route::view('/landing','landing');
+Route::view('/admin','admin.dashboard');
+
+Route::get('/teste-orm', function (){
+    User::create([
+        'name' => 'Ana Clara Santos',
+        'email' => 'ana.santos@escola.sp.gov.br',
+        'password' => '12345678'
+    ]);
+    return User::all();
+});
